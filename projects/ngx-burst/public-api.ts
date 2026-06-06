0158c1c8a850da7c4845b5cfe042916f781a24a7
Austin Kurtti <austin.kurtti@gmail.com>
2026-06-06 13:27:34 -0500

    Added element resets

--- projects/ngx-burst/public-api.ts
+++ projects/ngx-burst/public-api.ts
@@ -1,5 +1,6 @@
 /**
  * Main entry point ngx-burst
+ * TODO - consider restructuring lib as recommended here: https://angular.dev/reference/configs/file-structure#library-project-files
  */
 
 export const DUMMY = "ngx-burst-dummy";
